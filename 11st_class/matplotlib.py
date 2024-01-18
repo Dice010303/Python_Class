ddc30e118c8edcac89d3cba58128bbf3c5ac4c0b
MITE0303 <dice010303@gmail.com>
2024-01-18 16:26:30 +0900

    dice010303

--- 11st_class/matplotlib.py
+++ 11st_class/matplotlib.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-x_list = [i for i in range(1,11)]
-y_list = [i*2 for i in range(1,11)]
+x_list = [i for i in range(-5,6)]
+y_list = [i*i*-1 for i in range(-5,6 )]
 
 print(x_list)
 print(y_list)
