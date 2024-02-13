d22b277c3601e113caad6a7b5b3ea5cba58f8d81
MITE0303 <dice010303@gmail.com>
2024-02-13 15:36:11 +0900

    final

--- PyGame/pyGame.py
+++ PyGame/pyGame.py
@@ -3,6 +3,8 @@ import random
 # 난수 발생
 import time
 # 시간관련 모듈
+from datetime import datetime
+# 현재 시간 , 날짜 관련 모듈
 
 # 1. 초기화
 pygame.init()
@@ -22,6 +24,8 @@ clock = pygame.time.Clock()
 
 black = (0,0,0)
 white = (255,255,255)
+lime = (202,235,164)
+red = (255,0,0)
 
 # 충돌 판정 함수 -- crash__.png 확인
 def crash(a,b):
@@ -78,7 +82,30 @@ enemy_list = []
 
 k = 0  # 미사일 간격 변수
 
+score = 0  # enemy 객체 제거시 증가
+miss = 0  # enemy 객체 화면 밖으로 이탈시 증가
+
+game_over = 0
+font_wait = pygame.font.Font("C:/Users/최영준/AppData/Local/Microsoft/Windows/Fonts/D2Coding-Ver1.3.2-20180524-ligature.ttf",30)
+# 대기 화면(상황)
+wait_exit = 0
+while wait_exit == 0 :
+    clock.tick(60)
+    text_press = font_wait.render("press 'space'",True,lime)
+    screen.blit(text_press,(90,round(size[1]/2)))
+    for event in pygame.event.get():
+        # event.get() - 발생하는 이벤트 반환
+        # event에 발생하는 이벤트들을 저장
+        if event.type == pygame.QUIT: # [x]버튼을 누른 이벤트
+            pygame.quit()
+
+        if event.type == pygame.KEYDOWN: # 키가 눌린 경우
+            if event.key == pygame.K_SPACE:
+                wait_exit = 1
+    pygame.display.flip()
 # 4. 메인 이벤트 ( 코드 상에서 봤을 때의 이벤트)
+start_time = datetime.now() # 게임 시작 시간
+
 system_exit = 0  # 종료 시점 변수
 while system_exit == 0:
 
@@ -111,6 +138,9 @@ while system_exit == 0:
                 space_move = False
 
     #  - 4-3. 입력, 시간에 따른 변화
+    current_time = datetime.now() # 진행 되고 있는 중의 시간
+    delta_time = round((current_time - start_time).total_seconds())
+
     if left_move == True:
         player.x-=player.distance
         if player.x <= 0 : # 화면의 좌측 상한 0~
@@ -169,12 +199,13 @@ while system_exit == 0:
         e = enemy_list[i]
         e.y += e.distance
         if e.y >= size[1]: # enemy 객체의 y값이 화면을 벗어날 경우
-            delete_list.append(e) # 삭제 list 에 추가
+            delete_list.append(i) # 삭제 list 에 추가
 
     try:
         delete_list.reverse() # 가장먼저 list에 추가된것부터 삭제하기 위해 역순으로 순회
         for d in delete_list: # delete_list 순회(역순임)
-            del missile_list[d]
+            del enemy_list[d]
+            miss+=1
         # d 객체를 인덱스 자리에 써서 오류 발생! (Runtime Error) - 예외(exception)
     except:
         pass
@@ -196,13 +227,14 @@ while system_exit == 0:
     # set 자료형 {} - 집합 자료형 , 중복된 자료를 제거해 준다.
 
     try:
-        delete_missile_list.reverse()
+        delete_missile_list.reverse() # 먼저 충돌한 객체부터 제거
         delete_enemy_list.reverse()
         for dm in delete_missile_list:
             del missile_list[dm]
 
         for de in delete_enemy_list:
             del enemy_list[de]
+            score+=1
     except:
         pass
 
@@ -211,7 +243,8 @@ while system_exit == 0:
         e = enemy_list[i]
         if crash(e,player) == True:
             system_exit = 1   #  종료 변수
-            time.sleep(1) # 단위 : sec
+            game_over = 1
+            # time.sleep(1) # 단위 : sec
 
 
 #  - 4-4. 전사작업(그리기)
@@ -226,10 +259,32 @@ while system_exit == 0:
     for e in enemy_list: # enemy_list 순회 -> 화면에 출력
         e.show()
 
+    font = pygame.font.Font("C:/Users/최영준/AppData/Local/Microsoft/Windows/Fonts/D2Coding-Ver1.3.2-20180524-ligature.ttf",20)
+
+    text_score = font.render("score : {}".format(score),True,lime)
+    screen.blit(text_score,(10,10))
+
+    text_miss = font.render("miss : {}".format(miss),True,lime)
+    screen.blit(text_miss,(size[0]-100,10))
+
+    text_time = font.render("time = {}".format(delta_time),True,lime)
+    screen.blit(text_time,(150,10))
+    # True = 안티앨리어싱 여부
+
+
+
+
     #  - 4-5. 업데이트
     pygame.display.flip()
     # flip() - 화면에 나타냄
 # 5. 종료
-pygame.quit()
+if game_over == 1 or system_exit == 1:
+    screen.fill(white)
+    font_over = pygame.font.Font("C:/Users/최영준/AppData/Local/Microsoft/Windows/Fonts/D2Coding-Ver1.3.2-20180524-ligature.ttf",50)
+    text_over = font_over.render("game over",True,red)
+    screen.blit(text_over,(90,round(size[1]/2)-30))
+    pygame.display.flip()
+    time.sleep(2)
+    pygame.quit()
 # 반복문 탈출 -> 프로그램 종료
 # quit() - 종료
